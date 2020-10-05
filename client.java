04c5614dfda22a4c4c89e11a9f79f5dd016667f0
zlu <lulo9390@mylaurier.ca>
2020-10-05 16:11:48 -0400

    update

--- client.java
+++ client.java
@@ -1,16 +1,52 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
 
 public class client{
+	private JFrame frame;
+	private JPanel top, mid, bottom, portPanel, IPPanel, connectionPanel;
+	private JLabel portLabel;
+	private JTextField portTxtField;
+	
+	
 	public static void main(String[] args){
-		JFrame frame = new JFrame();
-		frame.setVisible(true);
-		Container contentPane;
-		contentPane = frame.getContentPane();
-		contentPane.setLayout(new BorderLayout());
-		frame.setSize(700, 700);
+		client newClient = new client();
+		newClient.setUpGUI();
+	}
+	
+	public void setUpGUI(){
+		frame = new JFrame("CP372 A1 - Zachary Luloff/Mitchell Mactaggart");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("CP372 A1 - Zachary Luloff/Mitchell Mactaggart");
+		
+		JPanel pane = new JPanel();
+		frame.setContentPane(pane);
+		pane.setLayout(new BorderLayout());
+		
+		top = new JPanel();
+		mid = new JPanel();
+		bottom = new JPanel();
+		top.setBackground(Color.blue);
+		pane.add(top, BorderLayout.NORTH);
+		pane.add(mid, BorderLayout.CENTER);
+		pane.add(bottom, BorderLayout.SOUTH);
+		
+		
+		portPanel = new JPanel();
+		portPanel.setSize(233,50);
+		IPPanel = new JPanel();
+		connectionPanel = new JPanel();
+		top.add(portPanel);
+		top.add(IPPanel);
+		top.add(connectionPanel);
+		
+		portLabel = new JLabel("Port: ");
+		portTxtField = new JTextField();
+		portTxtField.setSize(60, 10);
+		portPanel.add(portLabel);
+		portPanel.add(portTxtField);
+		
+		frame.setSize(800, 500);
+		frame.setVisible(true);
 	}
 }
